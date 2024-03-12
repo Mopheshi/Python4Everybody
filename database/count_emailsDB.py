d67dca867df92bf0ff63d2fd44f34f81e35ff564
mopheshi <ndachimya@gmail.com>
2024-03-12 14:02:17 +0100

    Counting emails in a DB

---
+++ database/count_emailsDB.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('CREATE TABLE Counts (email TEXT, count INTEGER)')
+
+fName = input('Enter file name: ')
+if len(fName) < 1: fName = 'mbozx-short.txt'
+
+fh = open(fName)
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1]
+    cur.execute('SELECT FROM Counts WHERE email = ?', (email,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('INSERT INTO Counts (email, count) VALUES (?, 1)', (email,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?', (email,))
+    conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
